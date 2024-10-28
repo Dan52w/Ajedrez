0b3e630e0350247b476137ab72bec5e9b391a76a
Dan <danflorezmartinez@gmail.com>
2024-10-28 18:35:41 -0500

    Se Agregaron comentarios respecto aal funcionamiento del codigo, asi mismo correccion de errores

--- src/main/java/org/example/pieza/Peon.java
+++ src/main/java/org/example/pieza/Peon.java
@@ -11,32 +11,36 @@ public class Peon extends Pieza{
 
     @Override
     public Boolean valido(int[] posI, int[] posS, Casilla casilla) {
-        if(casilla.getOcupada()){
-            if(!casilla.getPieza().getColor().equals(color) && !casilla.getPieza().getTipo().equals("Rey")){
-                if(posI[1] == posS[1]-1 || posI[1] == posS[1]+1){
-                    primerMovimiento = false;
-                    //moverPieza(posS);
+        if((posI[0] > 0 && posI[0] < 9)
+                && (posI[1] > 0 && posI[1] < 9)
+                && (posS[0] > 0 && posS[0] < 9)
+                && (posS[1] > 0 && posS[1] < 9)){ //Validar que el peon si se este moviendo dentro del tablero y no por fuera de este
+            if(casilla.getOcupada()){ //Para validar los casos en donde se capturen piezas
+                if(!casilla.getPieza().getColor().equals(color) && !casilla.getPieza().getTipo().equals("Rey")){
+                    if((posI[1] == posS[1]-1 || posI[1] == posS[1]+1) //Validar si es hacia la derecha o izquierda
+                            && posI[0] != posS[0] // validar que la pieza no este al frente
+                            && (posI[0] == posS[0]-1 || posI[0] == posS[0]+1)){ //Validar que el peon no se mueva mas de una casilla hacia arriba
+                        primerMovimiento = false;
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            } else if (!primerMovimiento) {
+                if((posS[0] == posI[0]+1) && posS[1] == posI[1]){ //Validar que el peon solo se mueva una casilla
                     return true;
                 }else{
                     return false;
                 }
-            }else{
-                return false;
-            }
-        } else if (!primerMovimiento) {
-            if(posS[0] == posI[0]+1){
-                //moverPieza(posS);
-                return true;
-            }else{
-                return false;
-            }
-        } else if (primerMovimiento) {
-            if(posS[0] == posI[0]+1 || posS[0] == posI[0]+2){
-                primerMovimiento = false;
-                //moverPieza(posS);
-                return true;
-            }else {
-                return false;
+            } else if (primerMovimiento) { //En caso de que sea el primer movimiento del peon permitirle mover dos casilla
+                if((posS[0] == posI[0]+1 || posS[0] == posI[0]+2) && posI[1] == posS[1]){
+                    primerMovimiento = false;
+                    return true;
+                }else {
+                    return false;
+                }
             }
         }
         return false;
