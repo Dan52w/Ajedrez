0cf2149cce5eed1d8740a5e58880b1952ef1f378
Dan <danflorezmartinez@gmail.com>
2024-12-05 11:33:39 -0500

    Main listo para ejecutar el programa y jugar el juego.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,19 @@
 package org.example;
 
-import org.example.tablero.Tablero;
+import org.example.partida.Partida;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Tablero tablero = new Tablero();
-        tablero.imprimirTablero();
+        Partida partida = new Partida();
+        Scanner scanner = new Scanner(System.in);
+        partida.getTablero().imprimirTableroJuego();
+        System.out.println("Introduce la posicion de la pieza que quieres mover: ");
+        String posI = scanner.nextLine();
+        System.out.println("Introduce a donde la quieres mover: ");
+        String posS = scanner.nextLine();
+        partida.jugar(posI, posS);
+
     }
 }
